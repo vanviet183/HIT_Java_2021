a099b43138027537024fb2540fec78329223f0a5
vanviet183 <vanvietgg183@gmail.com>
2021-10-12 22:56:24 +0700

    Java Hit 2021

--- BTVN Buoi 1/Bai1.java
+++ BTVN Buoi 1/Bai1.java
@@ -5,23 +5,19 @@ import java.util.Arrays;
 public class Bai1 {
     public static void main(String[] args) {
         int m, n;
-        m = 4;
+        m = 6;
+        n = 8;
+
         for(int i = 1; i <= m; i++) {
-            if(i % 2 != 0) {
-                n = 2;
-                System.out.print("   ");
-                for(int j = 1; j <= n; j++) {
-                    System.out.print("  *   ");
-                }
-            } else {
-                if(i < 3) {
-                    n = 3;
-                    for(int j = 1; j <= n; j++) {
-                        System.out.print("  *   ");
-                    }
+            for(int j = 1; j <= n; j++) {
+                if(i == 1 || i == m) {
+                    System.out.print("*");
                 } else {
-                    n = 1;
-                    System.out.print("        *");
+                    if(j == 1 || j == n) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
                 }
             }
             System.out.println();
